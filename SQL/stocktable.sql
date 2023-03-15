5cd34c64347d3cd85aceed375a6727a9ee0158dd
Vernis <102807886+yellowyfishes@users.noreply.github.com>
2023-03-15 22:00:53 +0800

    Add files via upload

---
+++ SQL/stocktable.sql
@@ -0,0 +1,8 @@
+/*source ~/myWebProject/tomcat/webapps/meshop/SQL/stocktable.sql*/
+
+insert into stock values (1001, 'Mr. Guide', 'Jurassic', 10.90, 50);
+insert into stock values (1002, 'Sir Paraglider', 'Jurassic', 10.90, 50);
+insert into stock values (1003, 'History Egg', 'Jurassic', 12.90, 50);
+insert into stock values (1004, 'Baby Dinosaur', 'Jurassic', 13.90, 50);
+insert into stock values (1005, 'Tour Ball', 'Jurassic', 15.90, 50);
+Select * from stock;
